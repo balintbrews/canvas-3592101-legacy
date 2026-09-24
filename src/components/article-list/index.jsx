c700e6ff91ec5570ef966be541083da465de0661
Balint Kleri <297418+balintbrews@users.noreply.github.com>
2026-09-24 18:43:20 +0000

    Remove date formatter from compatibility examples

--- src/components/article-list/index.jsx
+++ src/components/article-list/index.jsx
@@ -1,5 +1,4 @@
 import {
-  canvasFormatDateTime,
   cn,
   FormattedText,
   getNodePath,
@@ -90,9 +89,7 @@ export default function ArticleList({ resourceType, image, className }) {
                   {article.title}
                 </a>
               </h3>
-              <time dateTime={article.created}>
-                {canvasFormatDateTime(article.created)}
-              </time>
+              <time dateTime={article.created}>{article.created}</time>
               <FormattedText>{article.body.processed}</FormattedText>
             </li>
           ))}
